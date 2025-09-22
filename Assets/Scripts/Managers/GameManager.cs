4e995b013630864d5785dc00db5aa935ef4e9cc3
YitssTG <ismaelhuaman300@gmail.com>
2025-09-22 01:13:37 -0500

    Movement player - Cinemachine & TowerSlots

---
+++ Assets/Scripts/Managers/GameManager.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager Instance;
+
+    [Header("Torretas disponibles")]
+    public GameObject[] towerPrefabs;
+    private int selectedTowerIndex = 0;
+
+    void Awake()
+    {
+        if (Instance == null) Instance = this;
+        else Destroy(gameObject);
+    }
+
+    public bool CanBuild()
+    {
+        return towerPrefabs.Length > 0;
+    }
+
+    public GameObject GetSelectedTower()
+    {
+        return towerPrefabs[selectedTowerIndex];
+    }
+
+    public void SelectTower(int index)
+    {
+        if (index >= 0 && index < towerPrefabs.Length)
+            selectedTowerIndex = index;
+    }
+}
